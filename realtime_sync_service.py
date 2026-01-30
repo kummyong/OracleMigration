7cb8d9fe86499d97316f7c7f55bbeb1d9211f081
kummyong <kummyong@gmail.com>
2026-01-30 17:31:30 +0900

    Fix NameError: Add missing timedelta import in realtime_sync_service.py

--- realtime_sync_service.py
+++ realtime_sync_service.py
@@ -4,7 +4,7 @@ import json
 import time
 import threading
 import os
-from datetime import datetime
+from datetime import datetime, timedelta
 from concurrent.futures import ThreadPoolExecutor
 import concurrent.futures
 from logging.handlers import RotatingFileHandler
